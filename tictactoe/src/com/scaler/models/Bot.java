41f912737f90b47435c088567a3440584b48d2b5
riturajjain <riturajjain11@gmail.com>
2024-12-28 22:07:04 +0530

    easy bot working of tictactoe

--- tictactoe/src/com/scaler/models/Bot.java
+++ tictactoe/src/com/scaler/models/Bot.java
@@ -1,10 +1,36 @@
 package com.scaler.models;
 
+import com.scaler.strategies.BotPlayingStrategy;
+import com.scaler.strategies.impl.EasyBotPlayingStrategy;
+import com.scaler.strategies.impl.HardBotPlayingStrategy;
+import com.scaler.strategies.impl.MediumBotPlayingStrategy;
+
 public class Bot extends Player{
     private BotDifficultyLevel botDifficultyLevel;
-
+    private BotPlayingStrategy botPlayingStrategy;
     public Bot(String botName, String botSymbol, BotDifficultyLevel difficultyLevel) {
         super(botName,botSymbol,PlayerType.BOT);
         this.botDifficultyLevel = difficultyLevel;
+        this.botPlayingStrategy = getBotPlayingStrategy(botDifficultyLevel);
+    }
+
+    private BotPlayingStrategy getBotPlayingStrategy(BotDifficultyLevel botDifficultyLevel) {
+        switch (botDifficultyLevel){
+            case EASY -> {
+                return new EasyBotPlayingStrategy();
+            }
+            case MEDIUM -> {
+                return new MediumBotPlayingStrategy();
+            }
+            case HARD -> {
+                return new HardBotPlayingStrategy();
+            }
+        }
+        return new EasyBotPlayingStrategy();
+    }
+
+    @Override
+    public Move decideMove(Board board) {
+        return botPlayingStrategy.decideMove(this,board);
     }
 }
